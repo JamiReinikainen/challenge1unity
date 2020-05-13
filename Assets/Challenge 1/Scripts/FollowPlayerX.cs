d01579143b9ede2b3b62efc56369a3d116773a74
JamiReinikainen <gr189348@gradia.fi>
2020-05-13 14:16:08 +0300

    commited

---
+++ Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject Small_Plane02;
+    private Vector3 offset = new Vector3(22, 3, 9);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Small_Plane02.transform.position + offset;
+    }
+}
